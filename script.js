8cd5f7e5483f5d3033604352313941b97fab167f
Aidan Carey <aidan721@yahoo.com>
2022-08-20 20:29:16 -0400

    Col display, CSS + JS improvements
    Signed-off-by: Aidan Carey <aidan721@yahoo.com>

--- script.js
+++ script.js
@@ -16,14 +16,25 @@ function compute()
         // calculate remaining vars
         var interest = principal * years * rate / 100;
         var year = new Date().getFullYear()+parseInt(years);
-        var total = principal + interest;
+        var amount = principal + interest;
         
+        // Round monetrary values to nearest hundreth
+        interest = money_round(interest);
+        amount = money_round(amount);
+
         // Add commas for improved user readability
         interest = numberWithCommas(interest);
-        principal = numberWithCommas(principal)
-        total = numberWithCommas(total)
+        principal = numberWithCommas(principal);
+        amount = numberWithCommas(amount);
     
-        document.getElementById("result").innerHTML="If you deposit $\<mark\>"+principal+"\</mark\>,\<br\>at an interest rate of \<mark\>"+rate+"%\</mark\>\<br\>You will receive an interest amount of \<br\>$\<mark\>"+interest+"\</mark\>, in the year \<mark\>"+year+"\</mark\> \<br\> for a total of $\<mark\>"+total+"\</mark\>.\<br\>";
+        // document.getElementById("result").innerHTML="If you deposit $\<mark\>"+principal+"\</mark\>,\<br\>at an interest rate of \<mark\>"+rate+"%\</mark\>\<br\>You will receive an interest amount of \<br\>$\<mark\>"+interest+"\</mark\>, in the year \<mark\>"+year+"\</mark\> \<br\> for a total of $\<mark\>"+amount+"\</mark\>.\<br\>";
+        document.getElementById("principal_txt").innerHTML = principal;
+        document.getElementById("interest").innerHTML = interest;
+        document.getElementById("amount").innerHTML = amount;
+        document.getElementById("year").innerHTML = year;
+        document.getElementById("years_txt").innerHTML = years;
+        document.getElementById("rate_txt").innerHTML = rate;
+
         window.update()
     }
     
@@ -35,6 +46,10 @@ function updateRate()
     var rateval = document.getElementById("rate").value;
     document.getElementById("rate_val").innerText=rateval;
     window.update()
+}
+
+function money_round(num) {
+    return Math.ceil(num * 100) / 100;
 }
 
 // automatically add commas to monetary values
